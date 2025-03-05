302a5995812809aa6f22bc9cee581a1dae4a940a
Junsoo Kim <kjsoo1901@gmail.com>
2025-03-05 22:11:53 +0900

    2문제
    백준 수학 브론즈1 2문제

---
+++ Boj9506.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Boj9506 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        List<Integer> divisors;
+        int divisorSum;
+
+        while (true){
+            divisors = new ArrayList<>();
+            divisorSum = 0;
+            num = sc.nextInt();
+            sc.nextLine();
+
+            if(num == -1){
+                break;
+            }
+
+            // 약수 구하기
+            for(int i = 1; i < num; i++){
+                if(num % i == 0){
+                    divisors.add(i);
+                    divisorSum += i;
+                }
+            }
+
+            if(divisorSum != num){
+                System.out.println(num + " is NOT perfect");
+            } 
+            else {
+                System.out.print(num + " = " + divisors.get(0));
+                for(int i = 1; i < divisors.size(); i++){
+                    System.out.print(" + " + divisors.get(i));
+                }
+                System.out.println();
+            }
+        }
+
+        sc.close();
+    }
+}
